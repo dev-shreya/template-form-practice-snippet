c96e2266830e10dbaeebf01d0174241d1e57aec7
dev-shreya <shreyaabond005@gmail.com>
2024-06-23 01:19:16 -0700

    reset form in orignal state after submit

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { FormsModule, NgForm } from "@angular/forms";
 
 @Component({
@@ -9,11 +9,15 @@ import { FormsModule, NgForm } from "@angular/forms";
     imports:[FormsModule]
 })
 export class HeaderComponent{
+  @ViewChild('f') signupForm !: NgForm;
     defaultGender='';
     answer='';
     ageGroup=['Below 18','Above 18'];
     defaultAgeGroup = 'Above 18';
+
+
     onSubmit(form:NgForm){
       console.log(form)
+      this.signupForm.reset();
     }
 }
