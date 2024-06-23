1f4fce83819d61906189ca53095528772def5b86
dev-shreya <shreyaabond005@gmail.com>
2024-06-23 01:30:19 -0700

    Submitted data visible on screen

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -14,10 +14,20 @@ export class HeaderComponent{
     answer='';
     ageGroup=['Below 18','Above 18'];
     defaultAgeGroup = 'Above 18';
+    userData={
+      email:'',
+      password:'',
+      gender:'',
+      age:''
+    }
 
 
     onSubmit(form:NgForm){
       console.log(form)
+      this.userData.email=this.signupForm.value.email;
+      this.userData.password=this.signupForm.value.password;
+      this.userData.gender=this.signupForm.value.gender;
+      this.userData.age=this.signupForm.value.age;
       this.signupForm.reset();
     }
 }
